741950d8da9ac92526525d76099d76f119a4e4b1
edwardmartin1 <edwardmartin1@yahoo.com>
2025-11-04 20:15:36 -0600

    commit branch2 - creating mock data

---
+++ vite-unit-1-final-project/src/test-data/mockVolunteerRegistrations.js
@@ -0,0 +1,22 @@
+export const mockVolunteerRegistrations = 
+[
+    {
+        registrationId: 500,
+        userId: 100,
+        eventId: 1,
+        taskId: 1,
+    },
+    {
+        registrationId: 501,
+        userId: 100,
+        eventId: 2,
+        taskId: 1,
+    },
+    {
+        registrationId: 502,
+        userId: 100,
+        eventId: 3,
+        taskId: 1,
+    },
+];
+
