31c62fcb6b38b0bdf369e759348ba6426c9760fd
edwardmartin1 <edwardmartin1@yahoo.com>
2025-11-11 18:22:25 -0600

    fixed grammar

--- vite-unit-1-final-project/src/components/pages/HomePage.jsx
+++ vite-unit-1-final-project/src/components/pages/HomePage.jsx
@@ -94,7 +94,7 @@ const HomePage = ({allVolunteerEvents,
               <p>Do you enjoy helping and enriching the lives of others?</p>
               <p>Bessie’s Table provides an excellent opportunity to do both! Volunteering is simple. We need volunteers to assist with:</p>
               <p>View our <Link to="/volunteerevents">volunteer</Link> opportunities.</p>
-              <p>Bessie’s Table serves dinner each Monday from 5:30pm until 6:30pm.  Dining room volunteers should arrive by 4:00 pm to set up. Clean-up is completed by 7:00 pm.</p>
+              <p>Bessie’s Table serves dinner each Monday from 5:30 p.m. until 6:30 p.m.  Dining room volunteers should arrive by 4:00 pm to set up. Clean up is completed by 7:00 pm.</p>
 {/*
             <h2>Your Volunteer Registrations</h2>
             {allVolunteerRegistrations.length === 0 
